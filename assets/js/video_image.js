3d49bb1ee78028785a78befeb577f9a5814d17d4
Jonathan Séguin <2133662@etudiant.cegepvicto.ca>
2021-11-09 16:57:52 -0500

    ajout video yt img

---
+++ assets/js/video_image.js
@@ -0,0 +1,69 @@
+function VideoASC()
+{
+    
+    let imgASC= document.getElementById("asc_vid");
+    
+    
+
+    let iframe = '<iframe width="368px" height="234px" src="https://www.youtube.com/embed/kgNkGohA20k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+    imgASC.innerHTML=iframe;
+
+    
+}
+
+function VideoBG5()
+{
+    
+    let imgASC= document.getElementById("bg5_vid");
+    
+    
+
+    let iframe = '<iframe width="368px" height="234px" src="https://www.youtube.com/embed/hWnSZnkjZa4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+    imgASC.innerHTML=iframe;
+
+    
+}
+
+function VideoBOF()
+{
+    
+    let imgASC= document.getElementById("bof_vid");
+    
+    
+
+    let iframe = '<iframe width="368px" height="234px" src="https://www.youtube.com/embed/2ji-cLcT-UI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+    imgASC.innerHTML=iframe;
+
+    
+}
+
+function VideoKake()
+{
+    
+    let imgASC= document.getElementById("kake_vid");
+    
+    
+
+    let iframe = '<iframe width="368px" height="234px" src="https://www.youtube.com/embed/cTlHQiRNVl0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+    imgASC.innerHTML=iframe;
+
+    
+}
+
+function VideoTens()
+{
+    
+    let imgASC= document.getElementById("tens_vid");
+    
+    
+
+    let iframe = '<iframe width="368px" height="234px" src="https://www.youtube.com/embed/uOzwqb74K34" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+
+    imgASC.innerHTML=iframe;
+
+    
+}
